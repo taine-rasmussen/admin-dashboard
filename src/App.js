3df3fe6fd65e0f623f10491ed213be90ed73da26
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-17 22:34:29 +1200

    asdfasd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import react from 'react';
+
 const App = () => {
+
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const handleDate = () => {
+
+    console.log(data)
+
+  };
+
   return (
     <div className="App">
-      working
+      <button
+        onClick={handleDate}
+      >????</button>
     </div>
   );
 }
