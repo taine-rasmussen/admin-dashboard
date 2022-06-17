d565e99574f1bada42a95dbb0d01406a2247c69e
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-17 22:42:48 +1200

    removed ????

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import react from 'react';
 
 const App = () => {
 
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  const handleDate = () => {
-
-    console.log(data)
-
-  };
-
   return (
     <div className="App">
-      <button
-        onClick={handleDate}
-      >????</button>
     </div>
   );
 }
