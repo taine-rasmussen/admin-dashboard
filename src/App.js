093a06a8cf75d6c7b2aa2ffe0001e08feb84f7ad
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-19 16:03:56 +1200

    settings tooltip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import react, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-cons/fi'
-import { ToolTipComponent } from '@syncfusion/ej2-react-popups'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css';
 
 const App = () => {
 
   return (
     <div className="App">
-      <h1 className="underline text 3x1">App</h1>
+      <BrowserRouter >
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 " style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content='Settings'
+              position='Top'
+            >
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
